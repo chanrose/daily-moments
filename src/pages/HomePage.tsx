7e91bf225a8552079035acb55dd7f2910605598e
Sokin Keo <testing@email.com>
2020-11-17 13:34:09 +0700

    Updated entries page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -21,13 +21,12 @@ import { Entry, toEntry } from '../model';
 const HomePage: React.FC = () => {
   const {userId} = useAuth();
   console.log('This user is logged in homepage', userId);
-
   const [entries, setEntries] = useState<Entry[]>([]);
+
   useEffect(() => {
     const entriesRef = firestore.collection('users').doc(userId).collection('entries');
-    entriesRef
-      .get()
-      .then(({docs}) => setEntries(docs.map(toEntry)));
+    
+      return entriesRef.onSnapshot(({docs}) => setEntries(docs.map(toEntry)));
 
 /*     entriesRef.get().then((snapshot) => {
       const entries = snapshot.docs.map((doc) => ({
