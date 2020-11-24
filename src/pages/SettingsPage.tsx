9a95e376b9dd9dd40fd8fa68c6a07d9998d152ec
Chanbroset Prach <51664152+chanrose@users.noreply.github.com>
2020-11-24 13:41:38 +0000

    Test stopwatch function

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -4,12 +4,41 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-  IonButton
+  IonButton,
+  IonList,
+  IonCard,
+  IonText,
+  IonCardHeader,
+  IonCardContent
 } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
 import {auth} from '../firebase';
+import moment from 'moment';
 
 const SettingsPage: React.FC = () => {
+  const [timeIn, setTimeIn] = useState('');
+  const [timeOut, setTimeOut] = useState('');  
+  const [timeSaved, setTimeSaved] = useState('');
+
+  const handleTimeIn = () => {
+    const now = moment();
+    console.log("Time In: ", now);
+    setTimeIn(now.toISOString());    
+  }
+
+  const handleTimeOut = () => {
+    const now = moment();  
+    console.log("Time Out: ", now);
+    setTimeOut(now.toISOString());  
+  }
+
+  const handleSaveTime = () => {
+    const time1 = moment(timeIn);
+    const time2 = moment(timeOut);
+    const timeDiff = time2.diff(time1);
+    console.log(timeDiff/1000, "Second");
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -18,11 +47,21 @@ const SettingsPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
+        <IonCard>
+          <IonCardHeader><IonText>Stopwatch</IonText></IonCardHeader>
+          <IonCardContent>
+            <IonButton onClick={handleTimeIn} >Time In</IonButton>
+            <IonButton onClick={handleTimeOut} color="danger">Time Out</IonButton>
+            <IonButton onClick={handleSaveTime} color="secondary">Save Time</IonButton>
+          </IonCardContent>
+          
+        </IonCard>
         <IonButton 
           color="medium" 
           expand="block"
           onClick={() => auth.signOut()}>
             Logout</IonButton>
+        
       </IonContent>
     </IonPage>
   );
